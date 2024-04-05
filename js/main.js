ca1814bb6b7f1cf2132f4121b067d2e88e393fee
taachii <taachii21@outlook.com>
2024-04-06 00:13:03 +0200

    Move all functions to main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,71 @@
-const squares = document.querySelectorAll(".square");
 const clearButton = document.querySelector("#clear-button");
 const colorPicker = document.querySelector("#color-picker");
+const container = document.querySelector("div.container");
+const slider = document.querySelector("#grid-size-slider");
+
+const containerCompStyle = window.getComputedStyle(container)
+const containerWidth = parseInt(containerCompStyle.getPropertyValue("width"));
 
 let isMouseDown = false;
 let color = "blue";
 
-container.addEventListener("mousedown", () => {
-  isMouseDown = true;
-});
+let squares; 
+let gridSize;
 
-container.addEventListener("mouseup", () => {
-  isMouseDown = false;
-});
+init();
+setGridSize();
+generateGrid();
 
-squares.forEach(square => {
-  square.addEventListener("mousemove", addColor)
-});
+function init() {
+  slider.addEventListener("input", regenerateGrid);
 
-colorPicker.addEventListener("input", () => {
-    color = colorPicker.value;
-});
+  container.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+  
+  container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+  
+  colorPicker.addEventListener("input", () => {
+      color = colorPicker.value;
+  });
+  
+  clearButton.addEventListener("click", clear);
+}
 
-clearButton.addEventListener("click", clear);
+function setGridSize() {
+  let sliderValue = slider.value;
+  gridSize = sliderValue;
+}
+
+function generateGrid() {
+  for(let i = 0; i < gridSize**2; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    const squareSize = (containerWidth / gridSize) + "px";
+    div.style.width = squareSize;
+    div.style.height = squareSize;
+    container.appendChild(div);
+  }
+  squares = document.querySelectorAll(".square");
+  squares.forEach(square => {
+    square.addEventListener("mousemove", addColor);
+  });
+}
+
+function regenerateGrid() {
+  deleteAllSquares();
+  setGridSize();
+  generateGrid();
+}
+
+function deleteAllSquares() {
+  const squareCount = container.childElementCount;
+  for(let i = 0; i < squareCount; i++) {
+    container.removeChild(container.lastChild);
+  }
+}
 
 function addColor(e) {
   if(isMouseDown) {
